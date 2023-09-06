57a0026d8283ffa54cbd92eb5291f4f56aa1f372
molo06002 <molo06002@E-PW0234KQ>
2023-09-06 13:11:19 +0200

    Add project files.

---
+++ Uppgift_3_/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\"(; /\\;);\"");
+            Console.WriteLine("\"(; / \\\\;);\"");
+            Console.WriteLine("\"(; / \\;\\; \\\\;);\"");
+            Console.WriteLine("\"(;/______\\;);\"");
+        }
+    }
+}
